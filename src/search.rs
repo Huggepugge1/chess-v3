b04fa433d4d51a72af631599abf08d2d05a987de
Huggepugge1 <Huggepugge1@gmail.com>
2024-04-18 12:09:32 +0200

    stated alpha beta

--- src/search.rs
+++ src/search.rs
@@ -3,23 +3,23 @@ use crate::types::*;
 use crate::consts::*;
 use std::cmp::{min, max};
 
-pub fn evaluate(board: &Board) -> f32 {
-    let mut result: f32 = 0.0;
+pub fn evaluate(board: &Board) -> i32 {
+    let mut result = 0;
     for square in 0..64 {
         let piece = board.get_piece(square);
 
         result += match piece.typ {
-            PieceType::Pawn   => 1.008,
-            PieceType::Rook   => 4.961,
-            PieceType::Knight => 3.191,
-            PieceType::Bishop => 3.266,
-            PieceType::Queen  => 9.848,
-            PieceType::King   => 200.0,
-            PieceType::Empty  => 0.0,
+            PieceType::Pawn   => 1008,
+            PieceType::Rook   => 4961,
+            PieceType::Knight => 3191,
+            PieceType::Bishop => 3266,
+            PieceType::Queen  => 9848,
+            PieceType::King   => 200000,
+            PieceType::Empty  => 0,
         } * match piece.color {
-            Color::White => 1.0,
-            Color::Black => -1.0,
-            Color::Empty => 1.0,
+            Color::White => 1,
+            Color::Black => -1,
+            Color::Empty => 0,
         }
     }
 
@@ -44,10 +44,16 @@ fn is_legal_position(board: &mut Board, castling_bitboard: u64) -> bool {
 }
 
 pub fn search(depth: i32, board: &mut Board) -> Move {
+    let best =
+        if board.turn == Color::White {
+            i32::MIN
+        } else {
+            i32::MAX
+        };
     if depth == -1 {
-        min_max(4, board)[0].clone().0
+        min_max(4, board, best)[0].clone().0
     } else {
-        min_max(depth, board)[0].clone().0
+        min_max(depth, board, best)[0].clone().0
     }
 }
 
@@ -67,8 +73,6 @@ pub fn perft(start_depth: i32, depth: i32, board: &mut Board) -> i32 {
             }
         }
 
-        let board_copy = &board.clone();
-
         board.make_move(mov.clone());
         if is_legal_position(board, castling_bitboard) {
             let current_move = perft(start_depth, depth - 1, board);
@@ -78,23 +82,24 @@ pub fn perft(start_depth: i32, depth: i32, board: &mut Board) -> i32 {
                 println!("{}: {}", Board::print_move(&mov), current_move);
             }
         }
-        board.unmake_move(mov.clone());
-        if board != board_copy {
-            println!("{:?}", mov);
-            println!("OLD: {:?}", board_copy);
-            println!("NEW: {:?}", board);
-        }
+        board.unmake_move(mov);
     }
 
     result
 }
 
-fn min_max(depth: i32, board: &mut Board) -> Vec<(Move, f32)> {
+fn min_max(depth: i32, board: &mut Board, parent_score: i32) -> Vec<(Move, i32)> {
     if depth == 0 {
         vec![(EMPTY_MOVE, evaluate(board))]
     } else {
         let moves = board.generate_moves();
-        let mut result: Vec<(Move, f32)> = Vec::new();
+        let mut result = Vec::new();
+        let mut best =
+            if board.turn == Color::White {
+                i32::MIN
+            } else {
+                i32::MAX
+            };
 
         for mov in moves.clone() {
             let mut castling_bitboard = 0;
@@ -108,21 +113,36 @@ fn min_max(depth: i32, board: &mut Board) -> Vec<(Move, f32)> {
             board.make_move(mov.clone());
             
             if is_legal_position(board, castling_bitboard) {
-                let min_max_result = min_max(depth - 1, board);
+                let min_max_result = min_max(depth - 1, board, best);
 
                 if min_max_result.len() > 0 {
                     result.push((mov.clone(), min_max_result[0].1));
+                    
+                    if ()
+                    if board.turn == Color::White {
+                        best = i32::min(best, min_max_result[0].1);
+                        if parent_score > best {
+                            board.unmake_move(mov);
+                            break;
+                        }
+                    } else {
+                        best = i32::max(best, min_max_result[0].1);
+                        if parent_score < best {
+                            board.unmake_move(mov);
+                            break;
+                        }
+                    }
                 }
             }
 
-            board.unmake_move(mov.clone());
+            board.unmake_move(mov);
         }
 
         result.sort_by(|(_, score1), (_, score2)| 
             if board.turn == Color::White {
-                score1.partial_cmp(score2).unwrap()
-            } else {
                 score2.partial_cmp(score1).unwrap()
+            } else {
+                score1.partial_cmp(score2).unwrap()
             }
         );
 
